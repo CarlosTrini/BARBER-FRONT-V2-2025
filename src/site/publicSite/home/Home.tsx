8dc9658b190337873d605057cfcba0dc70830f5a
trinidadTvoai <ctrinidad@tvoai.com>
2025-05-10 17:55:10 -0600

    secciones del about

--- src/site/publicSite/home/Home.tsx
+++ src/site/publicSite/home/Home.tsx
@@ -1,8 +1,15 @@
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import useThemeStore from '../../../context/themeContext';
+import Services from './components/Services';
+
 import SlideOne from "../../../assets/img/front-1.png";
 import SlideTwo from "../../../assets/img/front-2.png";
 import SlideThree from "../../../assets/img/front-3.png";
-import { Swiper, SwiperSlide } from "swiper/react";
+import LogoDark from "../../../assets/img/logo-dark.png";
+import LogoLight from "../../../assets/img/logo-light.png";
+import ChainImg from "../../../assets/img/chair-2.jpg";
+import SccissorsImg from "../../../assets/img/sccissors-2.jpg";
 import './home.css';
 
 const imgsSlides = [
@@ -13,6 +20,8 @@ const imgsSlides = [
 
 
 const Home = () => {
+
+    const theme = useThemeStore(store => store.theme);
 
     return (
         <>
@@ -41,6 +50,45 @@ const Home = () => {
                             )
                         }
                     </Swiper>
+                </div>
+            </section>
+
+            <section className='b-bg-cream py-3 slogan-container'>
+                <div className='b-container-80 mx-auto flex md:gap-3 flex-col md:flex-row items-center md:items-start md:justify-center'>
+                    {/* <p className='b-text-cream  font-bold text-start'>Clásico de origen. Moderno por evolución...</p> */}
+                    <div className=' h-[100px]  w-[200px] my-4  bg-indigo-500 overflow-hidden rounded-md '>
+                        <img src={theme == 'light' ? LogoDark : LogoLight} alt={'imagen logotipo'} />
+                    </div>
+                    {/* <p className='b-text-cream font-bold md:self-end text-end'>Donde el pasado y el estilo se encuentran...</p> */}
+                </div>
+            </section>
+
+            <section className="b-bg-iron w-full">
+                <div className='md:flex h-[300px] '>
+                    <div className="border-about h-[100px] md:h-[100%] md:w-[50%] w-[100%] overflow-hidden">
+                        <img src={ChainImg} alt="imagen de silla de barbería" className='  object-cover' />
+                    </div>
+                    <div className='md:w-[80%] border-about h-[200px] md:h-[300px] px-5 md:w-[50%] w-[100%] flex items-center justify-center '>
+                        <p className='b-font-size-24 b-text-cream font-bold text-center'>Clásico de origen. Moderno por evolución...</p>
+                    </div>
+                </div>
+                <div className='md:flex h-[300px] '>
+                    <div className='md:w-[80%]  border-about h-[200px] md:h-[300px] px-5 md:w-[50%] w-[100%] flex items-center justify-center '>
+                        <p className='b-font-size-24 b-text-cream font-bold text-center'>Donde el pasado y el estilos se encuentran...</p>
+                    </div>
+                    <div className="border-about h-[100px] md:h-[100%] md:w-[50%] w-[100%] overflow-hidden">
+                        <img src={SccissorsImg} alt="imagen de silla de barbería" className='  object-cover' />
+                    </div>
+                </div>
+            </section>
+
+            <section id='servicesSection' className='b-bg-dark-full'>
+                <div className='b-container-85'>
+
+                    <div className='w-[250px] mx-auto'>
+                        <h2 className='sections-title b-text-cream py-5'>Servicios</h2>
+                    </div>
+                    <Services />
                 </div>
             </section>
         </>
---
+++ src/site/publicSite/home/components/Services.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { getAllServices } from "../../../../services/barberServices";
+
+
+const Services = () => {
+
+
+    const [services, setServices] = useState();
+
+    const getServices = async () => {
+        const response = await getAllServices();
+        console.log(response)
+    }
+
+    useEffect(() => {
+        getServices()
+    }, []);
+
+    return (
+        <>
+            <div className="md:flex gap-10 md:justify-evenly md:flex-wrap mt-[70px]" >
+             
+                <div className="relative flex flex-col rounded-xl b-bg-steel w-[100%]  lg:w-[47%] lg:max-w-[800px] mt-[50px] md:mt-3">
+                    <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl b-bg-gray  text-white shadow-lg  flex items-center justify-center">
+                    tesxto
+                    </div>
+                    <div className="p-6">
+                        <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                            Tailwind card
+                        </h5>
+                        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.
+                        </p>
+                    </div>
+                    <div className="p-6 pt-0">
+                        <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                            Read More
+                        </button>
+                    </div>
+                </div>
+               
+                
+               
+            </div>
+        </>
+    )
+}
+
+export default Services;
